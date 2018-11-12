04be57321ddc414d39e987054222fb2765bda294
SAM <braum.leblanc@hotmail.com>
2018-11-12 19:11:48 +0100

    ajouter_chevaux

--- joueur.c
+++ joueur.c
@@ -16,3 +16,17 @@ int nb_chevaux(joueur *p_joueur){ //le but est de trouver un moyen pour compter 
   }
   return incr;
 }
+
+void ajouter_chevaux_ecurie_init(joueur liste_joueur[], cheval j1, cheval j2, cheval j3, cheval j4){
+    int i =0;
+    while(liste_joueur[i].couleur != j1.couleur)
+    {
+      i+=1;
+    }
+
+    liste_joueur[i].liste_ecurie[0] = j1;
+    liste_joueur[i].liste_ecurie[1] = j2;
+    liste_joueur[i].liste_ecurie[2] = j3;
+    liste_joueur[i].liste_ecurie[3] = j4;
+
+}
