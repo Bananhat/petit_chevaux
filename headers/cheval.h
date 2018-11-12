26b4945011c3cb17b84982a8e9af9f9307de8a52
SAM <braum.leblanc@hotmail.com>
2018-11-12 19:12:48 +0100

    eaeaz

--- headers/cheval.h
+++ headers/cheval.h
@@ -6,7 +6,11 @@ enum couleur {'j', 'r', 'b','v'};
 
 typedef struct cheval {
   int case_x, case_y, numero;
+  int case_debut_x, case_debut_y;
+  int case_fin_x, case_fin_y;
+
   char couleur;
   char nom[2];
 }cheval;
+
 int deplacement(int *coord_x, int *coord_y, int val_D); // on utilise des pointeurs pour modifier directement les valeurs de coord_x ET coord_y
