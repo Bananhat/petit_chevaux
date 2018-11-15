e8d3cf785f30ae838ad6ecb28812e794552f6273
SAM <braum.leblanc@hotmail.com>
2018-11-15 11:18:37 +0100

    officiel

--- interface.c
+++ interface.c
@@ -18,24 +18,46 @@ void afficher_plateau(int size, char plateau[size][size])
 void init_partie(joueur* liste_joueur) {
   int nb_joueurs;
   char couleur_test;
-  printf("Entrez le nombre de joueur (minimum 2) :");
+  char couleur_use[4];
+  int deja_utiliser;
+
+  printf("Entrez le nombre de joueur (minimum 2) : ");
   scanf("%d", &nb_joueurs);
 
   while (getchar() != '\n');
 
   for(int i = 0; i < nb_joueurs; i++) {
+    printf("\n-------------------------------------------------------\n");
     do {
-    printf("Choisissez votre couleur: \n");
-    printf("Rouge(r), Bleu(b), Vert(v), Jaune(j)");
+      deja_utiliser =0;
+    printf("Choisissez votre couleur ( Rouge(r), Bleu(b), Vert(v), Jaune(j) ): ");
     scanf("%c", &couleur_test);
-    if (couleur_test == 'r' || couleur_test == 'v' || couleur_test == 'b' || couleur_test  == 'j'){
-      liste_joueur[i].couleur = couleur_test;
+    while(getchar() != '\n');
+
+    for(int i=0; i<4; i++){
+      if(couleur_use[i] == couleur_test){
+        deja_utiliser = 1;
+      }
     }
 
-  } while(couleur_test != 'r' || couleur_test != 'v' || couleur_test != 'b' || couleur_test != 'j');
+    if ((couleur_test == 'r' || couleur_test == 'v' || couleur_test == 'b' || couleur_test  == 'j') && deja_utiliser==0){
+      liste_joueur[i].couleur = couleur_test;
+      couleur_use[i] = couleur_test;
+    }
+    else{
+      if(deja_utiliser == 1){
+        printf("Cette couleur est déjà utilise..\n");
+      }
+      else{
+        printf("Ce n'est pas une couleur propose !!\ns");
+      }
+    }
+  } while( (couleur_test != 'r' && couleur_test != 'v' && couleur_test != 'b' && couleur_test != 'j') || deja_utiliser==1);
 
-    printf("Entrez votre nom : \n");
+    printf("Entrez votre nom : ");
     scanf("%s", liste_joueur[i].nom);
+    while(getchar() != '\n');
   }
+  printf("\n----------------------LA PARTIE COMMENCE-----------------------\n");
 
 }
