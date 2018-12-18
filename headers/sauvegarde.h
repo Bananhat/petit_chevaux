e281ef0ffd96acc70e6358a8a2ae92be4045bbe0
Gaudemer Samuel <21801540@C304C-UBU001>
2018-12-18 15:09:28 +0100

    doc official

--- headers/sauvegarde.h
+++ headers/sauvegarde.h
@@ -7,7 +7,7 @@
 *@param2 nom du fichier de sauvegarde à ouvrir ou créer
 *@param3 liste des joueurs
 *@param4 nombre de joueurs
-*@param2 liste contenant l'énumeration des couleurs
+*@param5 liste contenant l'énumeration des couleurs
 **/
 void sauvegarde_partie(char plateau[][15], char nom[30], joueur liste_joueur[], int nb_joueurs, int liste_couleurs[4]);
 /**
@@ -15,7 +15,7 @@ void sauvegarde_partie(char plateau[][15], char nom[30], joueur liste_joueur[], 
 *@param1 liste des joueurs
 *@param2 nom du fichier de sauvegarde à ouvrir
 *@param3 plateau de jeu
-*@param2 liste contenant l'énumeration des couleurs
+*@param4 liste contenant l'énumeration des couleurs
 **/
 int recup_sauvegarde(joueur liste_joueur[], char nom[30], char plateau[][15], int liste_couleurs[4]);
 
